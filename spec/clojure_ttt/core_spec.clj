10f15ceae1ae048afb7c3f6bfe5e72bf0ec5dcb8
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-01-18 16:25:45 -0600

    create-players test WIP

--- spec/clojure_ttt/core_spec.clj
+++ spec/clojure_ttt/core_spec.clj
@@ -2,44 +2,7 @@
   (:require [speclj.core :refer :all]
             [clojure-ttt.core :refer :all]))
 
-(describe "board-area"
-  (it "creates a board with a specified height and width"
-    (should= (vec (range 9)) (board-area 3 3))))
-
-(describe "marking the board"
-  (it "mark the board with a given marker on a specified spot"
-    (should= ["X" 1 2 3 4 5 6 7 8] (mark-spot "X" 0 [0 1 2 3 4 5 6 7 8]))
-    (should= ["X" 1 2 "O" 4 5 6 7 8] (mark-spot "O" 3 ["X" 1 2 3 4 5 6 7 8]))))
-
-(describe "winning horizontally"
-  (it "declares a winner with the correct number of adjacent horizontal markers"
-    (should= true (horizontal-winner? ["X" "X" "X" "O" 4 "O" 6 7 8]))
-    (should= nil (horizontal-winner? ["X" 1 "X" "O" 4 "O" 6 7 8]))))
-
-(describe "winning vertically"
-  (it "declares a winner the correct number of adjacent vertical markers"
-    (should= true (vertical-winner? ["X" 1 2 "X" 4 5 "X" 7 8]))
-    (should= nil (vertical-winner? ["X" 1 2 "O" 4 5 "X" 7 8]))))
-
-(describe "winning diagonally"
-  (it "declares a winner diagonally"
-    (should= true (diagonal-winner? ["X" 1 2 3 "X" 5 6 7 "X"]))
-    (should= nil (diagonal-winner? ["O" 1 2 "X" 4 5 "X" 7 8]))))
-
-(describe "tie game"
-  (it "declares a tie game"
-    (should= true (tie-game? ["X" "O" "X" "O" "O" "X" "X" "X" "O"]))
-    (should= false (tie-game? ["X" 1 2 "X" 4 5 "X" 7 8]))))
-
-(describe "game over"
-  (it "declares the end of the game"
-    (should= true (game-over? ["X" "O" "X" "O" "O" "X" "X" "X" "O"]))
-    (should= true (game-over? ["X" "X" "X" 3 4 5 6 7 8]))
-    (should= true (game-over? ["X" 1 2 "X" 4 5 "X" 7 8]))
-    (should= true (game-over? ["X" 1 2 3 "X" 5 6 7 "X"]))
-    (should= true (game-over? [0 1 "X" 3 "X" 5 "X" 7 8]))
-    (should= nil (game-over? [0 1 2 3 4 5 6 7 8]))))
-
-
-
-
+(describe "create-players"
+  (it "creates players for the game"
+    (should= [{:name "Jerome" :marker "X"} {:name "Griffin" :marker "O"}]
+     (create-players 2))))
--- src/clojure_ttt/core.clj
+++ src/clojure_ttt/core.clj
@@ -1,35 +1,10 @@
-(ns clojure-ttt.core)
+(ns clojure-ttt.core
+  (require [clojure-ttt.ui :refer :all]))
 
-(defn board-area [height width]
-  (vec (range (* height width))))
+(defn create-players [number]
+  (cond
+    (= 2 number) (let [players [{:name (prompt "Player 1, what is your name?") :marker (prompt "Select a marker (it can be any letter)")}
+                                {:name (prompt "Player 2, what is your name?") :marker (prompt "Select a marker (it can be any letter not chosen by player 1)")}]] players)))
 
-(defn mark-spot [marker spot board]
-  (assoc board spot marker))
 
-(defn is-won? [row]
-  (apply = row))
 
-(defn- has-winner? [lines]
-  (some true? (map is-won? lines)))
-
-(defn horizontal-winner? [board]
-  (let [rows (partition 3 board)]
-    (has-winner? rows)))
-
-(defn vertical-winner? [board]
-  (let [cols (->> board
-               (partition 3)
-               (apply interleave)
-               (partition 3))]
-    (has-winner? cols)))
-
-(defn diagonal-winner? [board]
-  (let [diag1 (map #(get board %) [0 4 8])
-        diag2 (map #(get board %) [2 4 6])]
-    (has-winner? [diag1 diag2])))
-
-(defn tie-game? [board]
-  (not-any? number? board))
-
-(defn game-over? [board]
-  (some #(% board) [horizontal-winner? vertical-winner? diagonal-winner? tie-game?]))
