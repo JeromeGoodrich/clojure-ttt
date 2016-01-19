b42e93f38e212d5210c88aaa3ebc077c6e5e0a46
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-01-19 13:26:57 -0600

    add functions and tests for create-players, make-move, end-game

--- spec/clojure_ttt/core_spec.clj
+++ spec/clojure_ttt/core_spec.clj
@@ -3,6 +3,28 @@
             [clojure-ttt.core :refer :all]))
 
 (describe "create-players"
-  (it "creates players for the game"
+
+  (it "creates 2 human players for the game"
+    (pending "don't want to go through output each time")
     (should= [{:name "Jerome" :marker "X"} {:name "Griffin" :marker "O"}]
-     (create-players 2))))
+     (create-players "2")))
+
+  (it "creates a human and computer player for the game"
+    (pending "don't want to go through output each time")
+    (should= [{:name "TicTacJoe" :marker "X"} {:name "Jerome" :marker "O"}]
+      (create-players "1"))))
+
+(describe "make-move"
+  (it "selects a spot on the board to move to"
+    (should= "5"
+      (with-in-str "5"
+        (make-move [{:name "Jerome" :marker "X"}])))))
+
+(describe "end-game"
+  (it "ends the game with a winner"
+    (should= "Game over! Jerome wins!"
+      (with-out-str (end-game [{:name "Jerome" :marker "X"}] [ "X" "X" "X" 3 4 5 6 7 8]))))
+
+  (it "ends the game as a tie"
+    (should= "Game over! It's a tie!"
+      (with-out-str (end-game [{:name "TicTacJoe" :marker "O"}] ["X" "O" "X" "O" "X" "X" "O" "X" "O"])))))
--- src/clojure_ttt/core.clj
+++ src/clojure_ttt/core.clj
@@ -1,10 +1,41 @@
 (ns clojure-ttt.core
-  (require [clojure-ttt.ui :refer :all]))
+  (:require [clojure-ttt.ui :refer :all]
+           [clojure-ttt.board :refer :all]))
 
 (defn create-players [number]
   (cond
-    (= 2 number) (let [players [{:name (prompt "Player 1, what is your name?") :marker (prompt "Select a marker (it can be any letter)")}
-                                {:name (prompt "Player 2, what is your name?") :marker (prompt "Select a marker (it can be any letter not chosen by player 1)")}]] players)))
+    (= "1" number) (let [players [{:name (prompt "What is your name?") :marker (prompt "Select a marker (it can be any letter)")}
+                                {:name "TicTacJoe" :marker (prompt "Select a marker for the computer")}]
+                       order (prompt "Who will go first? (me/comp)")]
+                    (cond
+                      (= order "me") players
+                      (= order "comp") (reverse players)))
 
+    (= "2" number) (let [players [{:name (prompt "Player 1, what is your name?")
+                                 :marker (prompt "Select a marker (it can be any letter)")}
+                                {:name (prompt "Player 2, what is your name?")
+                                 :marker (prompt "Select a marker (it can be any letter not chosen by player 1)")}]] players)))
+
+(defn make-move [player]
+  (cond
+    (= player :name "TicTacJoe") true
+    :else (prompt "Select a space using the numbers of the spaces above")))
+
+(defn end-game [player board]
+  (cond
+    (clojure-ttt.board/tie-game? board) (print "Game over! It's a tie!")
+    :else (print (str "Game over! " (:name (first player)) " wins!"))))
+
+;(defn game-loop
+;  (let [players (create-players (prompt "Welcome to TicTacToe! How many humans will be playing (1 or 2)?")
+;        board (board-size (prompt "What size board would you like to play on (3x3 = 3, 4x4 = 4, etc.)")
+;    (loop [players players
+;           board board]
+;      (if (board/game-over? board))
+;        (end-game board players)
+;          (recur (board/mark-spot (first players :marker)
+;                                  (make-move (first players))
+;                                  board)
+;             (reverse players))))
 
 
