725d27a127e7f8b070e2523f2fb544449ab25d23
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-01-28 17:31:32 -0600

    untested code for beatable ai

--- src/clojure_ttt/ai.clj
+++ src/clojure_ttt/ai.clj
@@ -15,24 +15,43 @@
 (defn min-by-score [scored-boards]
   (apply min-key :score scored-boards))
 
-(defn find-space-score [space board markers starting-marker]
+
+(defn find-space-score [space board markers starting-marker depth]
     (let [current-marker (first markers)]
-      (cond
+     (cond
         (and (win-game? board)
-             (= current-marker starting-marker)) (conj space {:score  10})
+             (= current-marker starting-marker)) (conj space {:score  (- 10 depth)})
         (and (win-game? board)
-             (not (= current-marker starting-marker))) (conj space {:score -10})
+             (not (= current-marker starting-marker))) (conj space {:score (+ depth -10)})
         (tie-game? board) (conj space {:score 0})
         :else (let [new-markers (reverse markers)
+                    depth (inc depth)
                     unmarked-spaces (find-unmarked-spaces board)
                     boards (create-possible-boards board unmarked-spaces new-markers)
-                    spaces-with-scores (map #(find-space-score space % new-markers starting-marker) boards)]
-                (if (not (= current-marker starting-marker))
-                  (max-by-score spaces-with-scores)
-                  (min-by-score spaces-with-scores))))))
+                    spaces-with-scores (map #(find-space-score space % new-markers starting-marker depth) boards)]
+                (cond
+                  (= depth 4) (conj space {:score 0})
+                  (not (= current-marker starting-marker)) (max-by-score spaces-with-scores)
+                  (= current-marker starting-marker) (min-by-score spaces-with-scores))))))
 
-(defn get-spaces-with-scores [spaces boards markers starting-marker]
-  (map #(find-space-score %1  %2 markers starting-marker) spaces boards))
+;(defn find-space-score [space board markers starting-marker]
+;    (let [current-marker (first markers)]
+;     (cond
+;        (and (win-game? board)
+;             (= current-marker starting-marker)) (conj space {:score  10})
+;        (and (win-game? board)
+;             (not (= current-marker starting-marker))) (conj space {:score -10})
+;        (tie-game? board) (conj space {:score 0})
+;        :else (let [new-markers (reverse markers)
+;                    unmarked-spaces (find-unmarked-spaces board)
+;                    boards (create-possible-boards board unmarked-spaces new-markers)
+;                    spaces-with-scores (map #(find-space-score space % new-markers starting-marker) boards)]
+;                (if (not (= current-marker starting-marker))
+;                  (max-by-score spaces-with-scores)
+;                  (min-by-score spaces-with-scores))))))
+
+(defn get-spaces-with-scores [spaces boards markers starting-marker depth]
+  (map #(find-space-score %1  %2 markers starting-marker depth) spaces boards))
 
 (defn ai-make-move [board markers]
  (if (= (count board) (count (find-unmarked-spaces board)))
@@ -41,6 +60,7 @@
         boards (create-possible-boards board unmarked-spaces markers)
         spaces (map #(hash-map :space %) unmarked-spaces)
         starting-marker (first markers)
-        spaces-with-scores (get-spaces-with-scores spaces boards markers starting-marker)]
+        spaces-with-scores (get-spaces-with-scores spaces boards markers starting-marker 0)]
+       (println spaces-with-scores)
     (:space (max-by-score spaces-with-scores)))))
 
