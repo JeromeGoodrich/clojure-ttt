4643326bc5802690526ecc19d6ef8ff1b45319db
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-02-01 10:17:25 -0600

    pass 85% test for AI

--- spec/clojure_ttt/ai_spec.clj
+++ spec/clojure_ttt/ai_spec.clj
@@ -4,7 +4,7 @@
             [clojure-ttt.board :refer :all]))
 
 
-(defn random-ai [board markers]
+(defn random-ai [board markers depth-limit]
   (rand-nth (find-unmarked-spaces board)))
 
 
@@ -18,10 +18,15 @@
       (if (= current-marker starting-marker)
         (let [unmarked-spaces (find-unmarked-spaces board)
               boards (create-possible-boards board unmarked-spaces markers)]
-          (map #(gen-all-boards % (reverse markers) starting-marker ai depth-limit) boards))
+          (flatten (map #(gen-all-boards % (reverse markers) starting-marker ai depth-limit) boards)))
         (let [move (ai board markers depth-limit)
               board (mark-spot (first markers) move board)]
           (gen-all-boards board (reverse markers) starting-marker ai depth-limit))))))
+
+(defn calc-winning [gen-all-boards]
+ (let [total (count (flatten (gen-all-boards)))
+       losing (count (filter false? (flatten (gen-all-boards))))]
+        (float (/ losing total))))
 
 (defn mock-game-loop [board markers]
   (cond
@@ -40,61 +45,61 @@
   (it "chooses the only available move"
     (should= 8  (ai-make-move ["X" "O" "X"
                                "X" "O" "O"
-                               "O" "X"  8] ["X" "O"])))
+                               "O" "X"  8] ["X" "O"] 8)))
 
   (it "selects a winning move over a non-winning move"
     (should= 6 (ai-make-move ["O" "X" "O"
                               "O" "X" "X"
-                               6   7  "X"] ["O" "X"])))
+                               6   7  "X"] ["O" "X"] 8)))
 
   (it "blocks the opponent from winning"
     (should= 6 (ai-make-move ["X" "O" "X"
                               "X" "O" "O"
-                               6  "X"  8 ] ["O" "X"])))
+                               6  "X"  8 ] ["O" "X"] 8)))
 
   (it "blocks the opponent from winning"
     (should= 8 (ai-make-move ["X" "O" "X"
                               "O" "O" "X"
-                               6  "X"  8 ] ["O" "X"])))
+                               6  "X"  8 ] ["O" "X"] 8)))
 
   (it "blocks the opponent from winning"
     (should= 1 (ai-make-move ["X"  1  "X"
                               "X" "O" "O"
-                              "O"  7   8] ["O" "X"])))
+                              "O"  7   8] ["O" "X"] 8)))
 
   (it "blocks the opponent from winning"
     (should= 1 (ai-make-move ["X" 1 "X"
                                3 "O" 5
-                               6  7  8] ["O" "X"])))
+                               6  7  8] ["O" "X"] 8)))
 
   (it "will prevent the opponent from making a 'fork'"
     (should= 6 (ai-make-move ["X"   1    2
                                3   "X"   5
-                               6    7   "O"] ["O" "X"])))
+                               6    7   "O"] ["O" "X"] 8)))
 
   (it "will win if given the chance"
     (should= 5 (ai-make-move ["X" 1 "O"
                                3 "X" 5
-                              "X" 7 "O"] ["O" "X"])))
+                              "X" 7 "O"] ["O" "X"] 8)))
 
   (it "will win if given the chance"
     (should= 3 (ai-make-move ["O" 1 "X"
                                3 "X" 5
-                              "O" 7 "X"] ["O" "X"])))
+                              "O" 7 "X"] ["O" "X"] 8)))
 
   (it "a random ai will lose if going through all possible game states"
-    (should= true  (some false? (flatten (gen-all-boards [0 1 2 3 4 5 6 7 8] ["X" "O"] "X" random-ai)))))
+    (should= true  (some false? (flatten (gen-all-boards [0 1 2 3 4 5 6 7 8] ["X" "O"] "X" random-ai 8)))))
 
   (it "minimax ai will never lose"
-    (should-not (some false? (flatten (gen-all-boards [0 1 2 3 4 5 6 7 8] ["X" "O"] "X" ai-make-move)))))
+    (should-not (some false? (flatten (gen-all-boards [0 1 2 3 4 5 6 7 8] ["X" "O"] "X" ai-make-move 8)))))
 
   (it "minimax ai will win around 85% of the time"
     ; call flatten within the gen-all-boards function, check if value lies within a range of 80 and 85.
-    (should== 83.6  (- 100 (* 100 (float (/ (count (filter false? (flatten (gen-all-boards [0 1 2 3 4 5 6 7 8] ["X" "O"] "X" ai-make-move 3))))
-                   (count(flatten (gen-all-boards [0 1 2 3 4 5 6 7 8] ["X" "O"] "X" ai-make-move 3)))))))))
+    (should-be (and #(> 85 %) #(< 80 %))  (- 100 (* 100 (float (/ (count (filter false? (gen-all-boards [0 1 2 3 4 5 6 7 8] ["X" "O"] "X" ai-make-move 3)))
+                                                                  (count (gen-all-boards [0 1 2 3 4 5 6 7 8] ["X" "O"] "X" ai-make-move 3))))))))
 
   (it "will move to a corner on the first move"
     (should= 8 (ai-make-move [0 1 2
                               3 4 5
-                              6 7 8] ["X" "O"]))))
+                              6 7 8] ["X" "O"] 8))))
 
